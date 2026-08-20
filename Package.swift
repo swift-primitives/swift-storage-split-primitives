e1441828955f07f6deffbc27a904dbcdf98d4ad7
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:49 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,24 +1,30 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-storage-split-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         // MARK: - Store.Split (the dual-plane store combinator)
-        .library(name: "Store Split Primitives", targets: ["Store Split Primitives"]),
+        .library(name: "Store Split Primitives", targets: ["Store Split Primitives"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-index-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-index-primitives.git",
+            branch: "main"
+        ),
         // Test-only: concrete Storage.Contiguous planes to compose under Store.Split.
-        .package(url: "https://github.com/swift-primitives/swift-storage-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-storage-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
 
@@ -37,7 +43,10 @@ let package = Package(
             name: "Store Split Primitives Tests",
             dependencies: [
                 "Store Split Primitives",
-                .product(name: "Storage Contiguous Primitives", package: "swift-storage-primitives"),
+                .product(
+                    name: "Storage Contiguous Primitives",
+                    package: "swift-storage-primitives"
+                ),
             ]
         ),
     ],
@@ -59,7 +68,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
     ]
 
     let package: [SwiftSetting] = [
-        .enableExperimentalFeature("RawLayout"),
+        .enableExperimentalFeature("RawLayout")
     ]
 
     target.swiftSettings = (target.swiftSettings ?? []) + ecosystem + package
